0d2843884e36814922645ff3e22478f9e7370a9b
ckorley4 <ckorley4uo@yahoo.com>
2024-03-28 06:37:19 -0400

    Lab Completed

--- client/src/components/NewMessage.js
+++ client/src/components/NewMessage.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 function NewMessage({ currentUser, onAddMessage }) {
-  const [body, setBody] = useState("");
+  const [body, setBody] = useState('')
 
   function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    fetch("http://127.0.0.1:4000/messages", {
-      method: "POST",
+    fetch('http://127.0.0.1:5555/messages', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         username: currentUser.username,
@@ -18,9 +18,9 @@ function NewMessage({ currentUser, onAddMessage }) {
     })
       .then((r) => r.json())
       .then((newMessage) => {
-        onAddMessage(newMessage);
-        setBody("");
-      });
+        onAddMessage(newMessage)
+        setBody('')
+      })
   }
 
   return (
@@ -34,7 +34,7 @@ function NewMessage({ currentUser, onAddMessage }) {
       />
       <button type="submit">Send</button>
     </form>
-  );
+  )
 }
 
-export default NewMessage;
+export default NewMessage
--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+import ipdb
 
 from models import db, Message
 
@@ -14,13 +15,31 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method =='GET':
+        messages = Message.query.all()
+        message_list = [message.to_dict() for message in messages]
+        return make_response(message_list,200)
+    elif request.method =='POST':
+       # ipdb.set_trace()
+        incoming = request.json
+        new_message = Message(**incoming)
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(new_message.to_dict(),201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET','PATCH','POST','DELETE'])
 def messages_by_id(id):
-    return ''
-
+    message = Message.query.get(id)
+    if request.method == 'GET':
+        return make_response(message.to_dict(),200)
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return make_response({},204)
+  
+    
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
